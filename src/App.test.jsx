da5f868f974a11ad1e9dc229277fef9486aac929
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2024-02-24 18:52:26 +0530

    Added more test cases

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,8 @@
-import {render, screen, waitFor} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
-import { expect, it, vi } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { act } from 'react-dom/test-utils';
-
+import FlightCard from './components/FlightCard';
 // I had less time left so wasn't able to write more test cases
 
 // Mock API
@@ -108,4 +108,108 @@ it('should have 2 flight cards', async () => {
     expect(flightCards.length).toBe(2);
 });
 
+describe('Flight Card', () => {
+    const details = {
+        "id": "1",
+        "fare": 3840,
+        "displayData": {
+          "source": {
+            "airport": {
+              "cityCode": "DEL",
+              "cityName": "Delhi",
+              "terminal": "3",
+              "airportCode": "DEL",
+              "airportName": "Indira Gandhi Airport",
+              "countryCode": "IN",
+              "countryName": "India"
+            },
+            "depTime": "2023-03-31T06:20"
+          },
+          "airlines": [
+            {
+              "airlineCode": "AB",
+              "airlineName": "JetSpice",
+              "flightNumber": "1234"
+            }
+          ],
+          "stopInfo": "Non stop",
+          "destination": {
+            "airport": {
+              "cityCode": "BOM",
+              "cityName": "Mumbai",
+              "terminal": "2",
+              "airportCode": "BOM",
+              "airportName": "Mumbai",
+              "countryCode": "IN",
+              "countryName": "India"
+            },
+            "arrTime": "2023-03-31T08:40"
+          },
+          "totalDuration": "2h 20m"
+        }
+      };
+    it('should have correct airline name', async () => {
+        render(<FlightCard details={details}/>);
+        expect(screen.getByText(details.displayData.airlines[0].airlineName).innerHTML).toBe('JetSpice');
+    });
 
+    it('should have correct airline code', async () => {
+        render(<FlightCard details={details}/>);
+        const airlineCode = `${details.displayData.airlines[0].airlineCode}-${details.displayData.airlines[0].flightNumber}`;
+        expect(screen.getByText(airlineCode).innerHTML).toBe('AB-1234');
+    });
+
+    it('should have correct source', async () => {
+        render(<FlightCard details={details}/>);
+        const source = `${details.displayData.source.airport.airportCode}, T${details.displayData.source.airport.terminal}`;
+        expect(screen.getByText(source).innerHTML).toBe('DEL, T3');
+    });
+
+    it('should have correct destination', async () => {
+        render(<FlightCard details={details}/>);
+        const destination = `${details.displayData.destination.airport.airportCode}, T${details.displayData.destination.airport.terminal}`;
+        expect(screen.getByText(destination).innerHTML).toBe('BOM, T2');
+    });
+
+    it('should have correct duration', async () => {
+        render(<FlightCard details={details}/>);
+        expect(screen.getByText(details.displayData.totalDuration).innerHTML).toBe('2h 20m');
+    });
+
+    it('should have correct stop information', async () => {
+        render(<FlightCard details={details}/>);
+        expect(screen.getByText(details.displayData.stopInfo).innerHTML).toBe('Non stop');
+    });
+
+    it('should have correct price', async () => {
+        render(<FlightCard details={details}/>);
+        const fare = `Rs. ${details.fare}/-`
+        expect(screen.getByText(fare).innerHTML).toBe('Rs. 3840/-');
+    });
+
+    it('should have correct departure time', async () => {
+        render(<FlightCard details={details}/>);
+        const depTime = new Date(details.displayData.source.depTime);
+        const timeString = `${depTime.getHours()}:${depTime.getMinutes()}`;
+        expect(screen.getByText(timeString).innerHTML).toBe('6:20');
+    });
+
+    it('should have correct arrival time', async () => {
+        render(<FlightCard details={details}/>);
+        const arrTime = new Date(details.displayData.destination.arrTime);
+        const timeString = `${arrTime.getHours()}:${arrTime.getMinutes()}`;
+        expect(screen.getByText(timeString).innerHTML).toBe('8:40');
+    });
+    
+    it('should have correct source airport', async () => {
+        render(<FlightCard details={details}/>);
+        const arrAirport = `${details.displayData.source.airport.airportName}, ${details.displayData.source.airport.cityName}, ${details.displayData.destination.airport.countryName}`;
+        expect(screen.getByText(arrAirport).innerHTML).toBe('Indira Gandhi Airport, Delhi, India');
+    });
+
+    it('should have correct destination airport', async () => {
+        render(<FlightCard details={details}/>);
+        const arrAirport = `${details.displayData.destination.airport.airportName}, ${details.displayData.destination.airport.cityName}, ${details.displayData.destination.airport.countryName}`;
+        expect(screen.getByText(arrAirport).innerHTML).toBe('Mumbai, Mumbai, India');
+    });
+})
